58613284016551d7aeac1fa45f760fdbc6285250
odetineerajreddy-web <odetineerajreddy@gmail.com>
2026-08-27 10:09:06 +0530

    add

--- Practice/M03_Object_Oriented_Programing Concepts/S03/polymorphism.py
+++ Practice/M03_Object_Oriented_Programing Concepts/S03/polymorphism.py
@@ -19,8 +19,20 @@ print(add(10,20,30,40))'''
 Python does not support Function overloading directly we can achieve this variable length arguments
 '''
 
-def add(*values):
+'''def add(*values):
     return sum(values)
 print(add(10,20))
 print(add(10,20,30))
-print(add(10,20,30,40))
+print(add(10,20,30,40))'''
+
+#Operator Overloading
+class Add:
+    def __init__(self,x):
+        self.x=x
+    def __add__(self,val):
+        return self.x + val.x
+
+a = Add(10)
+b = Add(20)
+print(a+b)
+print(a-b)
